0d1192fb0badb6ebbf85678de192df44ccf71f1b
Melina Ulloa <melinaulloa98@gmail.com>
2025-06-29 14:43:55 -0400

    Cambios locales: enviroments y ajustes en app.module/package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,38 @@
-// Librerías globales
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-// Shared: Contenido global
+// IMPORTAMOS COMPONENTES GLOBALES
 import { SharedModule } from './modules/shared/shared.module';
 
+// FIREBASE -> importamos HERRAMIENTAS de la Base de Datos
+import { environment } from '../enviroments/enviroments'; // vincula a la BD con la APP
+import { AngularFireModule } from '@angular/fire/compat'; // trabaja con las colecciones de información
+import { AngularFireAuthModule } from '@angular/fire/compat/auth'; // trabaja con la autentificación
+import { AngularFireStorageModule } from '@angular/fire/compat/storage'; // trabaja con imágenes y archivos
+
+/**
+ * npm install firebase --force <- fuerza la instalación
+ * npm install @angular/fire --save --force
+ */
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    BrowserAnimationsModule,
+    // COMPONENTES GLOBALES
+    SharedModule,
+    // VINCULACIÓN CON FIREBASE
+    AngularFireModule.initializeApp(environment.firebaseConfig), // Inicializar Firebase dentro del proyecto
+    AngularFireAuthModule,
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
